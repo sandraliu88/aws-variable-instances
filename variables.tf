5c9047b36e5a4ff378cb071c55bea030a28c7ade
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2022-07-29 09:40:54 -0700

    Update variables.tf
    reducing the number of subnets, but increasing instance type.

--- variables.tf
+++ variables.tf
@@ -9,10 +9,10 @@ variable "project" {
   type        = map(any)
   default = {
     website-dev = {
-      public_subnet_count  = 2,
-      private_subnet_count = 2,
+      public_subnet_count  = 1,
+      private_subnet_count = 1,
       instances_per_subnet = 2,
-      instance_type        = "t2.micro",
+      instance_type        = "t2.large",
       environment          = "dev"
     },
   }
