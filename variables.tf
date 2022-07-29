cf21363b970f1bd77a5390c644c8fd7c85843a4e
Omid Eghaneyan <oeghaneyan@gmail.com>
2022-07-28 20:44:48 -0700

    rename projects and limit them to 1

--- variables.tf
+++ variables.tf
@@ -8,20 +8,13 @@ variable "project" {
   description = "Map of project names to configuration"
   type        = map(any)
   default = {
-    project-alpha = {
+    website-dev = {
       public_subnet_count  = 2,
       private_subnet_count = 2,
       instances_per_subnet = 2,
       instance_type        = "t2.micro",
       environment          = "dev"
     },
-    project-beta = {
-      public_subnet_count  = 1,
-      private_subnet_count = 1,
-      instances_per_subnet = 2,
-      instance_type        = "t2.nano",
-      environment          = "test"
-    }
   }
 }
 
