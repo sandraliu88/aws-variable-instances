17a88f998893ab786551cce2e1b99518a038a2ae
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-04-04 10:15:40 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "project" {
     website-dev = {
       public_subnet_count  = 1,
       private_subnet_count = 1,
-      instances_per_subnet = 3,
+      instances_per_subnet = 2,
       instance_type        = "t2.micro",
       environment          = "dev"
     },
